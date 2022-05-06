3b2ece0e959ebc46b79763192db9452457911c83
Gianmarco G <giangarof98@gmail.com>
2022-05-06 12:04:14 -0400

    movie finder with jq done

--- JQ/movies/app.js
+++ JQ/movies/app.js
@@ -59,6 +59,15 @@ function getDetails(){
                     </div>
                     <div>
                         <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                            <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                            <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                            <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                            <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                            <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                        </ul>
                     </div>
                     <a href="./movie.html" class="btn">Go Back</a>
                 </div>
