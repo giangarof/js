97d6650f7c0207c046d2cf3ba99115be5220bc29
Gianmarco G <giangarof98@gmail.com>
2022-06-10 13:08:13 -0400

    exports issue

--- TSC/ticTacToe/dist/index.js
+++ TSC/ticTacToe/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const board = document.getElementById('game-container');
 let turn = 'X';
 function listenBoard() {
@@ -17,6 +18,46 @@ function runGame(e) {
     if (box === null || box.textContent !== '')
         return;
     box.textContent = turn;
+    const winner = checkWinner();
+    if (!winner)
+        switchPlayer();
+    else {
+        console.log('winner');
+    }
+}
+function checkWinner() {
+    const boxes = getBoxes();
+    return ((boxes[0] === boxes[1] && boxes[1] === boxes[2] && boxes[0] !== '') ||
+        (boxes[3] === boxes[4] && boxes[4] === boxes[5] && boxes[3] !== '') ||
+        (boxes[6] === boxes[7] && boxes[7] === boxes[6] && boxes[6] !== '') ||
+        (boxes[0] === boxes[4] && boxes[4] === boxes[2] && boxes[0] !== '') ||
+        (boxes[2] === boxes[4] && boxes[4] === boxes[2] && boxes[2] !== '') ||
+        (boxes[1] === boxes[4] && boxes[4] === boxes[2] && boxes[1] !== '') ||
+        (boxes[0] === boxes[3] && boxes[3] === boxes[2] && boxes[0] !== '') ||
+        (boxes[2] === boxes[5] && boxes[5] === boxes[2] && boxes[2] !== ''));
+}
+function getBoxes() {
+    const boxesContent = [];
+    for (let i = 0; i < 9; i++) {
+        const box = document.getElementById(`box-${i}`);
+        console.log(box);
+        const boxContent = box.textContent;
+        if (boxContent === null) {
+            boxesContent.push('');
+        }
+        else {
+            boxesContent.push(boxContent);
+        }
+    }
+    return boxesContent;
+}
+function switchPlayer() {
+    if (turn === 'X') {
+        turn = 'O';
+    }
+    else {
+        turn = 'X';
+    }
 }
 function createBoard() {
     for (let i = 0; i < 9; i++) {
--- TSC/ticTacToe/src/index.ts
+++ TSC/ticTacToe/src/index.ts
@@ -1,3 +1,5 @@
+import { isNonNullExpression } from "typescript";
+
 const board = document.getElementById('game-container') as HTMLDivElement
 type Turn = 'X' | 'O' | ''
 let turn : Turn = 'X'
@@ -12,16 +14,54 @@ function main(): void{
     listenBoard()
 }
 
-function runGame(e: Event):void{
+function runGame(e: Event): void{
     const boxId: string | null = (<HTMLElement>e.target).id;
     console.log(boxId)
     if( boxId === null) return;
     const box:  HTMLElement | null = document.getElementById(`${boxId}`)
-    if(box === null || box.textContent !== '') return
-    box.textContent = turn
+    if(box === null || box.textContent !== '') return;
+    box.textContent = turn;
+    const winner: boolean = checkWinner();
+    if(!winner) switchPlayer()
+    else { console.log('winner')}
 
+}
 
+function checkWinner(): boolean{
+    const boxes: string[] = getBoxes();
+    return(
+        (boxes[0] === boxes[1] && boxes[1] === boxes[2] && boxes[0] !== '') ||
+        (boxes[3] === boxes[4] && boxes[4] === boxes[5] && boxes[3] !== '') ||
+        (boxes[6] === boxes[7] && boxes[7] === boxes[6] && boxes[6] !== '') ||
+        (boxes[0] === boxes[4] && boxes[4] === boxes[2] && boxes[0] !== '') ||
+        (boxes[2] === boxes[4] && boxes[4] === boxes[2] && boxes[2] !== '') ||
+        (boxes[1] === boxes[4] && boxes[4] === boxes[2] && boxes[1] !== '') ||
+        (boxes[0] === boxes[3] && boxes[3] === boxes[2] && boxes[0] !== '') ||
+        (boxes[2] === boxes[5] && boxes[5] === boxes[2] && boxes[2] !== '')
+    )
+}
 
+function getBoxes():string[]{
+    const boxesContent: string[] = [];
+    for (let i = 0; i<9; i++){
+        const box = document.getElementById(`box-${i}`) as HTMLElement;
+        console.log(box)
+        const boxContent: string | null = box.textContent;
+        if(boxContent === null){
+             boxesContent.push('');
+        } else{
+            boxesContent.push(boxContent)
+        }
+    }
+    return boxesContent
+}
+
+function switchPlayer(): void{
+    if(turn === 'X'){
+        turn = 'O'
+    } else {
+        turn = 'X'
+    }
 }
 
 function createBoard():void{
