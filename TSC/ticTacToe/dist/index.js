7773eecfeea52a1eb3a4fe49fd549ec07b502b6a
Gianmarco G <giangarof98@gmail.com>
2022-06-09 20:43:55 -0400

    grid-boxes

--- TSC/ticTacToe/dist/index.js
+++ TSC/ticTacToe/dist/index.js
@@ -1,11 +1,16 @@
 "use strict";
 const gameContainer = document.getElementById('game-container');
 function main() {
-    makeBox();
+    createBoard();
 }
-function makeBox() {
+function createBoard() {
+    for (let i = 0; i < 9; i++)
+        makeBox(i);
+}
+function makeBox(i) {
     const box = document.createElement('div');
     box.className = 'box';
+    box.id = `box-${i}`;
     box.textContent = 'X';
     gameContainer.append(box);
 }
--- TSC/ticTacToe/src/index.ts
+++ TSC/ticTacToe/src/index.ts
@@ -1,12 +1,19 @@
 const gameContainer = document.getElementById('game-container') as HTMLDivElement
 
 function main(): void{
-    makeBox();
+    createBoard();
 }
 
-function makeBox():void{
+function createBoard():void{
+    for(let i = 0; i < 9; i++)makeBox(i)
+    
+
+}
+
+function makeBox(i:number):void{
     const box:HTMLDivElement = document.createElement('div');
     box.className = 'box';
+    box.id = `box-${i}`
     box.textContent = 'X';
     gameContainer.append(box);
 }
