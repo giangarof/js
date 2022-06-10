dcb3006f031bb6e1718eb30449a8768b58cc8a53
Gianmarco G <giangarof98@gmail.com>
2022-06-10 12:14:43 -0400

    event and type

--- TSC/ticTacToe/dist/index.js
+++ TSC/ticTacToe/dist/index.js
@@ -1,7 +1,22 @@
 "use strict";
-const gameContainer = document.getElementById('game-container');
+const board = document.getElementById('game-container');
+let turn = 'X';
+function listenBoard() {
+    board.addEventListener('click', runGame);
+}
 function main() {
     createBoard();
+    listenBoard();
+}
+function runGame(e) {
+    const boxId = e.target.id;
+    console.log(boxId);
+    if (boxId === null)
+        return;
+    const box = document.getElementById(`${boxId}`);
+    if (box === null || box.textContent !== '')
+        return;
+    box.textContent = turn;
 }
 function createBoard() {
     for (let i = 0; i < 9; i++) {
@@ -12,7 +27,7 @@ function makeBox(i) {
     const box = document.createElement('div');
     box.className = 'box';
     box.id = `box-${i}`;
-    box.textContent = 'X';
-    gameContainer.append(box);
+    box.textContent = '';
+    board.append(box);
 }
 main();
--- TSC/ticTacToe/src/index.ts
+++ TSC/ticTacToe/src/index.ts
@@ -1,23 +1,41 @@
-const gameContainer = document.getElementById('game-container') as HTMLDivElement
+const board = document.getElementById('game-container') as HTMLDivElement
+type Turn = 'X' | 'O' | ''
+let turn : Turn = 'X'
+
+
+function listenBoard(): void{
+    board.addEventListener('click', runGame)
+}
 
 function main(): void{
     createBoard();
+    listenBoard()
+}
+
+function runGame(e: Event):void{
+    const boxId: string | null = (<HTMLElement>e.target).id;
+    console.log(boxId)
+    if( boxId === null) return;
+    const box:  HTMLElement | null = document.getElementById(`${boxId}`)
+    if(box === null || box.textContent !== '') return
+    box.textContent = turn
+
+
+
 }
 
 function createBoard():void{
     for(let i = 0; i < 9; i++){
         makeBox(i)
     }
-    
-
 }
 
 function makeBox(i:number):void{
     const box:HTMLDivElement = document.createElement('div');
     box.className = 'box';
     box.id = `box-${i}`;
-    box.textContent = 'X';
-    gameContainer.append(box);
+    box.textContent = '';
+    board.append(box);
 }
 
 main()
