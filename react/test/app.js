a7a6f4adbda0714a3ec07716404fce649e3201f8
Gianmarco G <giangarof98@gmail.com>
2022-09-20 18:58:18 -0400

    react folder

---
+++ react/test/app.js
@@ -0,0 +1,16 @@
+const myNum = () => {
+    return Math.floor(Math.random()*44)+1
+}
+class Third extends React.Component {
+    render(){
+        const num = myNum()
+        return(
+            <div>
+                <h1>{num}</h1>
+                <p>{num % 2 ? 'odd' : 'even'}</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Third/>, document.getElementById('root'))
