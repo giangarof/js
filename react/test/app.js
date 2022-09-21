c4863f682de36628bd86583918f221831480b717
Gianmarco G <giangarof98@gmail.com>
2022-09-20 22:24:30 -0400

    component

--- react/test/app.js
+++ react/test/app.js
@@ -1,16 +1,10 @@
-const myNum = () => {
-    return Math.floor(Math.random()*44)+1
-}
-class Third extends React.Component {
+class App extends React.Component{
     render(){
-        const num = myNum()
         return(
             <div>
-                <h1>{num}</h1>
-                <p>{num % 2 ? 'odd' : 'even'}</p>
+                <Hello/>
             </div>
         )
     }
 }
-
-ReactDOM.render(<Third/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
